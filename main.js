c3d643902cdfd84634ce95718c56a30819fdf961
pranavhole <pranavhole050610@gmail.com>
2022-08-27 00:55:11 +0530

    123

--- main.js
+++
@@ -1,8 +0,0 @@
-let rocket = document.getElementById('rocket');
-window.addEventListener('scroll',()=>{
-    let value = window.scrollY;
-    rocket.style.top=600-(value*3)+'px';
-    rocket.style.left=(value*8)-120+'px';
-
-})
-
